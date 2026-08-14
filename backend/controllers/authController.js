49ef2b3a3088b24393ad0c6e367cb2c128624107
akashzone <akashnadar.dev@gmail.com>
2026-08-14 10:55:33 +0530

    feat: implement password hashing and comparison with bcrypt

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,18 +1,24 @@
-
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt")
 const register = async (req, res) => {
-  
-  const { email, username, password, country } = req.body;
+  console.log("req.body :", req.body);
   try {
+    const { email, username, password, country } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(409).json({
+        message: "User already exists",
+      });
+    }
+    const hash = bcrypt.hashSync(req.body.password, 5);
     const newUser = new User({
-      email,
-      userName: username,
-      password,
-      country,
+      ...req.body,
+      password: hash
     });
     await newUser.save();
     res.status(201).json({
-      message: "Successfully acc created",
+      message: "Successfully created",
       status: true,
     });
   } catch (error) {
@@ -24,19 +30,27 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
   try {
-    const userExist = await User.find({
-      email,
+    const user = await User.findOne({
+      email : req.body.email
     });
-    if (!userExist) {
-      res.status(500).json({
-        message: "User not found, register first..",
+    if (!user) {
+      res.status(404).json({
+        message: "User not found.",
         status: false,
       });
     }
+
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if(!isCorrect) return res.status(400).json({
+      message : "Wrong password or email",
+      status: false
+    })
+
+    const { password, ...info } = user._doc;
     res.status(200).json({
-      message: "Successfully acc fetched",
+      message: "Successfully account fetched",
+      info,
       status: true,
     });
   } catch (error) {
